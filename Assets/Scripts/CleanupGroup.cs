f420a55bb73a33457d25b4ef522e73ac42cbede5
Fabio <fabio.voelkner@gmail.com>
2026-04-24 06:20:06 +0200

    fix cleanup

--- Assets/Scripts/CleanupGroup.cs
+++ Assets/Scripts/CleanupGroup.cs
@@ -7,16 +7,12 @@ public class CleanupGroup : MonoBehaviour
 {
     [SerializeField] private CleanupItem[] items;
 
-    [Tooltip("Dialogue when all items are cleaned up.")]
-    [SerializeField] private DialogueData allCleanedDialogue;
-
     public UnityEvent OnAllCleaned;
 
     private int _cleanedCount = 0;
 
     void Start()
     {
-        // Auto-find items if not assigned
         if (items == null || items.Length == 0)
             items = GetComponentsInChildren<CleanupItem>();
     }
@@ -26,11 +22,6 @@ public class CleanupGroup : MonoBehaviour
         _cleanedCount++;
 
         if (_cleanedCount >= items.Length)
-        {
-            if (allCleanedDialogue != null)
-                DialogueManager.Instance.PlaySimpleDialogue(allCleanedDialogue);
-
             OnAllCleaned?.Invoke();
-        }
     }
 }
